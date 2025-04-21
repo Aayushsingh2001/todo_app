6674f74a193cc5836827dae727c7daa878977b85
Aayushsingh2001 <as6994770@gmail.com>
2025-04-22 05:26:45 +0530

    Added docker file to containerizing the TodoApp

--- todo-backend/start.js
+++ todo-backend/start.js
@@ -2,5 +2,5 @@ const app = require("./server")
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
-    console.log('Server is running on port', PORT)
+    console.log(`Server is running on port ${PORT}`)
 })
